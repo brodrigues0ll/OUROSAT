c4ea81e5c2e87ef4dab9dd7809cde988f0f2691c
brodrigues0ll <bernardorg99@hotmail.com>
2023-06-23 03:40:42 -0300

    Test Transform to PWA

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require("next-pwa")({
   dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
-
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ["aniyuki.com"],
   },
-});
+};
+
+module.exports = withPWA(nextConfig);
