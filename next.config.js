ef52341cd3f59c59408de58a601afa7474c08627
brodrigues0ll <bernardorg99@hotmail.com>
2023-06-29 00:51:48 -0300

    Alterando next.config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withPWA = require("next-pwa")({
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["aniyuki.com", "xvidios.xxx"],
+    domains: ["aniyuki.com", "i.pinimg.com"],
   },
 };
 
